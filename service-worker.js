87bf20804345ae540eda0df3086a82c1a86e6bb2
Laison12 <bartolomeucruz28@gmail.com>
2025-09-16 16:57:52 +0100

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,23 +1,24 @@
-const CACHE_NAME = "laison-app-v1";
-const URLS_TO_CACHE = [
-  "/",
-  "/index.html",
-  "/style.css",
-  "/script.js"
+const CACHE_NAME = "laison-info-v1";
+const ASSETS = [
+  "./",
+  "./index.html",
+  "./manifest.json",
+  "./icon-192.png",
+  "./icon-512.png"
 ];
 
-self.addEventListener("install", event => {
+self.addEventListener("install", (event) => {
+  event.waitUntil(caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS)));
+});
+
+self.addEventListener("activate", (event) => {
   event.waitUntil(
-    caches.open(CACHE_NAME).then(cache => {
-      return cache.addAll(URLS_TO_CACHE);
-    })
+    caches.keys().then((keys) =>
+      Promise.all(keys.filter((key) => key !== CACHE_NAME).map((key) => caches.delete(key)))
+    )
   );
 });
 
-self.addEventListener("fetch", event => {
-  event.respondWith(
-    caches.match(event.request).then(response => {
-      return response || fetch(event.request);
-    })
-  );
+self.addEventListener("fetch", (event) => {
+  event.respondWith(caches.match(event.request).then((resp) => resp || fetch(event.request)));
 });
